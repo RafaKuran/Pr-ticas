848572abf83e5a837d22cf651f9ea54fcf3348ec
Rafaela Miranda <34078718+RafaKuran@users.noreply.github.com>
2018-12-11 15:13:25 -0200

    Update centroide.py

--- centroide.py
+++ centroide.py
@@ -1,1 +1,22 @@
-'''O programa a seguir'''
+'''O programa a seguir é um complemento do exercício CriandoArq, e faz a leitura do arquivo "pontos.txt", com
+pontos 2D, com coordenadas (x,y). Calcula e escreve o centroide de todos os pontos lidos.'''
+
+#Subprograma
+def centroide(nome):
+  arquivo = open(nome, "r")
+  qtdPontos = 0
+  somaX = 0
+  somaY = 0
+  for coordenada in arquivo:
+    partes = coordenada.split()
+    somaX += float(partes[0])
+    somaY += float(partes[1])
+    qtdPontos += 1
+    if qtdPontos == 0:
+      print(arquivo.name, "-vazio!!!")
+    else:
+      print("Ponto calculado: (", somaX/qtdPontos, ",", somaY/qtdPontos, ")." )
+     return None
+  
+#Programa Principal
+centroide("pontos.txt")
